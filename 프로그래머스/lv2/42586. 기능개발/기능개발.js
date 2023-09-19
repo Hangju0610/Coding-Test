058f324477ae429f762fd5eeb3d0e403f074423b
Hyoung Joo Park <phj950610@naver.com>
2023-09-19 21:26:34 +0900

    [level 2] Title: 기능개발, Time: 2.52 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -0,0 +1,18 @@
+function solution(progresses, speeds) {
+    let result = []
+    while(progresses.length != 0){
+        // 하루 진행속도 추가
+         for (i = 0; i < progresses.length; i++) {
+             progresses[i] = progresses[i] + speeds[i]
+         }
+        let count = 0
+        while(progresses[0] >= 100) {
+            console.log(progresses[0])
+            progresses.shift()
+            speeds.shift()
+            count++
+        }
+        if (count > 0) result.push(count)
+    }
+    return result
+}
