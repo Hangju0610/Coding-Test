1f7a96cb535eb8361887d12c0b6797d5b1cd45a8
Hyoung Joo Park <phj950610@naver.com>
2023-09-20 15:43:19 +0900

    [level 2] Title: 전화번호 목록, Time: 389.17 ms, Memory: 98.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,13 @@
+function solution(phone_book) {
+  const table = {};
+  for (const number of phone_book) {
+    table[number] = true;
+  }
+  for (const number of phone_book) {
+    for (let i = 1; i < number.length; i += 1) {
+      const prefix = number.slice(0, i);
+      if (table[prefix]) return false;
+    }
+  }
+  return true;
+}
