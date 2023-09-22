c0034e7a49b3fc8c2ca889598d03413544380b68
Hyoung Joo Park <phj950610@naver.com>
2023-09-23 01:46:38 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,8 +1,10 @@
 function solution(n, words) {
     for(i = 1; i<words.length; i++) {
+        // 끝말잇기 단어가 다른 경우
         if(words[i-1][words[i-1].length-1] != words[i][0]){
             return [(i%n) + 1, Math.ceil((i+1)/n)]
         }
+        // 중복된 단어가 나올 경우
         if(words.indexOf(words[i]) != i){
             return [(i%n) + 1, Math.ceil((i+1)/n)]
         }
