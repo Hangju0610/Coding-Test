50067e698c2ac470694800478b6f4bef703b72cb
Hyoung Joo Park <phj950610@naver.com>
2023-12-28 11:41:21 +0900

    [level 2] Title: 타겟 넘버, Time: 1.13 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,4 +1,16 @@
 function solution(numbers, target) {
-    var answer = 0;
-    return answer;
+  let answer = 0;
+
+  function dfs(level, sum) {
+    if (level === numbers.length) {
+      if (sum === target) answer++;
+      return;
+    }
+    dfs(level + 1, sum + numbers[level]);
+    dfs(level + 1, sum - numbers[level]);
+  }
+
+  dfs(0, 0);
+
+  return answer;
 }
