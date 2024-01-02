c2399d364d3f1cf7392fabea7c7185be512c728e
Hyoung Joo Park <phj950610@naver.com>
2024-01-02 21:42:35 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,9 +1,9 @@
 function DFS(computers, node, visited) {
   // 현재 방문한 node true 처리
-  visited[node] = true;
+  visited.add(node);
   for (let j = 0; j < computers.length; j++) {
     // 한번도 들어가지 않은 노드 & 연결이 되어 있다면?
-    if (computers[node][j] == 1 && !visited[j]) {
+    if (computers[node][j] == 1 && !visited.has(j)) {
       DFS(computers, j, visited);
     }
   }
@@ -12,11 +12,11 @@ function DFS(computers, node, visited) {
 function solution(n, computers) {
   // DFS를 통해 구현
   // 이미 지나간 것은 넘어가도록 구현
-  const visited = new Array(n).fill(false);
+  const visited = new Set();
   let network = 0;
   for (let i = 0; i < n; i++) {
     // 이미 탐색한 node라면 반복문 통과
-    if (visited[i]) {
+    if (visited.has(i)) {
       continue;
     } else {
       DFS(computers, i, visited);
