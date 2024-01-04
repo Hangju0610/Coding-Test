9d326722601a5e4230f0b75203d6d45af5ed2a30
Hyoung Joo Park <phj950610@naver.com>
2024-01-04 23:32:24 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -0,0 +1,33 @@
+function solution(number, k) {
+  let numberArray = number.split("");
+  let stack = [];
+
+  for (let i = 0; i < numberArray.length; i++) {
+    while (
+      k > 0 &&
+      stack.length > 0 &&
+      stack[stack.length - 1] < numberArray[i]
+    ) {
+      stack.pop();
+      k--;
+    }
+    stack.push(numberArray[i]);
+  }
+
+  // 'k'가 여전히 남아있다면 가장 뒷부분의 숫자 제거
+  while (k > 0) {
+    stack.pop();
+    k--;
+  }
+
+  // 앞부분의 0 제거
+  while (stack.length > 0 && stack[0] === "0") {
+    stack.shift();
+  }
+
+  // 스택이 비어있다면 0을 반환
+  return stack.length === 0 ? "0" : stack.join("");
+}
+
+
+
