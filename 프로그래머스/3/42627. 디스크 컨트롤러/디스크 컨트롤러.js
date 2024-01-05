f947994647ffba88af28bad235a774018e5fb9c1
Hyoung Joo Park <phj950610@naver.com>
2024-01-05 15:32:59 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -1,86 +1,22 @@
 function solution(jobs) {
-  const count = jobs.length;
-  const minHeap = new MinHeap();
-  jobs.sort((a,b) => a[0]-b[0]);
-  
-  let time = 0;
-  let complete = 0;
-  let total = 0;
-  
-  while(jobs.length || minHeap.size()) {
-    while(jobs.length) {
-      if(jobs[0][0] === time) {
-        minHeap.heappush(jobs.shift());
-      } else break;
-    }
-    
-    if(minHeap.size() && time >= complete) {
-      const task = minHeap.heappop();
-      complete = task[1] + time;
-      total += complete - task[0];
-    }
-    time++;
-  }
-  
-  return total / count >> 0;
-}
-
-class MinHeap {
-    constructor() {
-        this.heap = [ null ];
-    }
-    
-    size() {
-        return this.heap.length - 1;
-    }
-    
-    getMin() {
-        return this.heap[1] ? this.heap[1] : null;
-    }
-    
-    swap(a, b) {
-        [ this.heap[a], this.heap[b] ] = [ this.heap[b], this.heap[a] ];
-    }
-    
-    heappush(value) {
-        this.heap.push(value);
-        let curIdx = this.heap.length - 1;
-        let parIdx = (curIdx / 2) >> 0;
-        
-        while(curIdx > 1 && this.heap[parIdx][1] > this.heap[curIdx][1]) {
-            this.swap(parIdx, curIdx)
-            curIdx = parIdx;
-            parIdx = (curIdx / 2) >> 0;
+    jobs.sort(([a, b], [c, d]) => a - c || b - d);
+    const waiting = [];
+    const count = jobs.length;
+    let processed_time = 0;
+    let time = 0;
+    while (jobs.length + waiting.length) {
+        let in_process;
+        while (jobs.length && (jobs[0][0] <= time)) {
+            waiting.push(jobs[0] && jobs.shift());
         }
-    }
-    
-    heappop() {
-        const min = this.heap[1];	
-        if(this.heap.length <= 2) this.heap = [ null ];
-        else this.heap[1] = this.heap.pop();   
-        
-        let curIdx = 1;
-        let leftIdx = curIdx * 2;
-        let rightIdx = curIdx * 2 + 1; 
-        
-        if(!this.heap[leftIdx]) return min;
-        if(!this.heap[rightIdx]) {
-            if(this.heap[leftIdx][1] < this.heap[curIdx][1]) {
-                this.swap(leftIdx, curIdx);
-            }
-            return min;
+        if (waiting.length) {
+            in_process = waiting.sort(([a, b], [c, d]) => b - d || a - c).shift();
+        } else {
+            in_process = jobs.shift();
+            time = in_process[0];
         }
-
-        while(this.heap[leftIdx][1] < this.heap[curIdx][1] || this.heap[rightIdx][1] < this.heap[curIdx][1]) {
-            const minIdx = this.heap[leftIdx][1] > this.heap[rightIdx][1] ? rightIdx : leftIdx;
-            this.swap(minIdx, curIdx);
-            curIdx = minIdx;
-            leftIdx = curIdx * 2;
-            rightIdx = curIdx * 2 + 1;
-            
-            if(leftIdx >= this.size()) break;
-        }
-
-        return min;
+        time += in_process[1];
+        processed_time += time - in_process[0];
     }
+    return Math.floor(processed_time / count);
 }
