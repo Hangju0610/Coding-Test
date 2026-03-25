4ac124c20ed0220a91a49ace80d243725ec512ea
Hyoung Joo Park <phj950610@naver.com>
2026-03-25 23:39:41 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -1,30 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 int n, r, temp, root;
 vector<int> adj[54];
-int dfs(int here){
+
+int dfs(int here) {
     int ret = 0;
     int child = 0;
-    for(int there : adj[here]){
-        if(there == r) continue;
+    for (int there : adj[here]) {
+        if (there == r)
+            continue;
         ret += dfs(there);
         child++;
     }
-    if(child == 0) return 1;
+    if (child == 0)
+        return 1;
     return ret;
 }
-int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     cin >> n;
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         cin >> temp;
-        if(temp == -1)root = i;
-        else adj[temp].push_back(i);
+        if (temp == -1) {
+            root = i;
+        } else {
+            adj[temp].push_back(i);
+        }
     }
+
     cin >> r;
-    if(r == root){
-        cout << 0 << "\n";return 0;
+    if (r == root) { // 반례상황
+        cout << 0 << '\n';
+        return 0;
     }
-    cout << dfs(root) << "\n";
+    cout << dfs(root) << '\n';
     return 0;
 }
