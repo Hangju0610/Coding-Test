dcfd77cbc061733b9e57086756e0b58a531a174c
Hyoung Joo Park <phj950610@naver.com>
2026-04-02 00:05:04 +0900

    [Gold IV] Title: 알파벳, Time: 1664 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -10,7 +10,7 @@ int dx[4] = {0, 1, 0, -1};
 
 int go(int y, int x, int cnt) {
     cnt++;
-    int result = cnt;
+    int result = cnt; // 현재 경로 길이를 result로 분리
     for (int i = 0; i < 4; i++)
     {
         int ny = y + dy[i];
@@ -19,13 +19,16 @@ int go(int y, int x, int cnt) {
         if(ny < 0 || ny >= r || nx < 0 || nx >= c || mp[a[ny][nx]] == 1)
             continue;
         mp[a[ny][nx]] = 1;
-        result = max(result, go(ny, nx, cnt));
+        result = max(result, go(ny, nx, cnt)); //cnt는 변경하지 않는다.
         mp[a[ny][nx]] = 0;
     }
     return result;
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     cin >> r >> c;
     for (int i = 0; i < r; i++) {
         cin >> s;
