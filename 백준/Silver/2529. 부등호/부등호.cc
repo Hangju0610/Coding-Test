185298f7aff0429dba0850c9d769bf701b4f46d9
Hyoung Joo Park <phj950610@naver.com>
2026-04-02 14:24:26 +0900

    [Silver I] Title: 부등호, Time: 4 ms, Memory: 3576 KB -BaekjoonHub

---
+++ 백준/Silver/2529. 부등호/부등호.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+int k, check[10];
+char op[10];
+vector<string> ret;
+
+bool good(char x, char y, char op) {
+    if (x > y && op == '>')
+        return true;
+    if (x < y && op == '<')
+        return true;
+    return false;
+}
+
+void go(int idx, string num) {
+    if (idx == k + 1) {
+        ret.push_back(num);
+        return;
+    }
+
+    for (int i = 0; i <= 9; i++) {
+        if (check[i])
+            continue;
+        if (idx == 0 || good(num[idx - 1], i + '0', op[idx - 1])) {
+            check[i] = 1;
+            go(idx + 1, num + to_string(i));
+            check[i] = 0;
+        }
+    }
+    return;
+}
+
+int main() {
+    cin >> k;
+    for (int i = 0; i < k; i++) {
+        cin >> op[i];
+    }
+    go(0, "");
+    sort(ret.begin(), ret.end());
+    cout << ret[ret.size() - 1] << '\n'
+         << ret[0] << '\n';
+    return 0;
+}
