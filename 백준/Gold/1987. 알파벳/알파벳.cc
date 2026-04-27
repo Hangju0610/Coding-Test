dc891e800cc547cd6ce0eb127f412e50ff6073c5
Hyoung Joo Park <phj950610@naver.com>
2026-04-27 11:18:11 +0900

    [Gold IV] Title: 알파벳, Time: 392 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,44 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int r, c;
-char a[21][21];
-map<char, int> mp;
+int r, c, visited[20][20], ret;
+char a[20][20];
 string s;
-int dy[4] = {-1, 0, 1, 0};
-int dx[4] = {0, 1, 0, -1};
 
-int go(int y, int x, int cnt) {
-    cnt++;
-    int result = cnt; // 현재 경로 길이를 result로 분리
-    for (int i = 0; i < 4; i++)
-    {
+const int dy[4] = {-1, 0, 1, 0};
+const int dx[4] = {0, 1, 0, -1};
+
+void go(int y, int x, int num, int cnt) {
+    ret = max(ret, cnt);
+    for (int i = 0; i < 4; i++) {
         int ny = y + dy[i];
         int nx = x + dx[i];
-
-        if(ny < 0 || ny >= r || nx < 0 || nx >= c || mp[a[ny][nx]] == 1)
-            continue;
-        mp[a[ny][nx]] = 1;
-        result = max(result, go(ny, nx, cnt)); //cnt는 변경하지 않는다.
-        mp[a[ny][nx]] = 0;
+        if(ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
+        int _next = (1 << (int)(a[ny][nx] - 'A'));
+        if ((num & _next) == 0)
+            go(ny, nx, num | _next, cnt + 1);
     }
-    return result;
+    return;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
     cin >> r >> c;
     for (int i = 0; i < r; i++) {
         cin >> s;
-        for (int j = 0; j < c; j++) {
+        for (int j = 0; j < s.size(); j++) {
             a[i][j] = s[j];
         }
     }
-
-    mp[a[0][0]] = 1;
     
-    int cnt = go(0, 0, 0);
-    cout << cnt;
+    go(0, 0, 1 << (int)(a[0][0] - 'A'), 1);
+    cout << ret << '\n';
+    return 0;
 }
